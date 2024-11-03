e27d4cc93a22444cfb1d56efb067a7e29fe44d7e
Dheeraj Goswami <dheerajgoswami2004@gmail.com>
2024-11-03 11:43:26 +0530

    Resetted Files

--- Test-Git/index.js
+++ Test-Git/index.js
@@ -2,3 +2,12 @@ let var1 = 10;
 let var2  20;
 
 console.log(var1 + var2);
+
+
+function func1(num1, num2) {
+	return num1 + num2;
+}
+
+functin func2(num1, num2) {
+	return num1 + num2;
+}
---
+++ Test-Git/util.js
@@ -0,0 +1,7 @@
+let var1 = 20;
+let var2 = 30;
+
+function add(num1, num2 ) {
+	return num1 + num2;
+}
+
